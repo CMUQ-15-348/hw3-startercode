5b727e6707949d673e9d6f7dd1ebdbb2517b8612
Ryan Riley <rriley.cs@gmail.com>
2022-08-29 10:35:38 +0300

    Initial HW3 starter code

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "15348.h"
+#include "timer.h"
+#include "serial.h"
+
+void PLLInit()
+{
+    SYSCTL_RCC2_R |= 0x80000000;
+    SYSCTL_RCC2_R |= 0x00000800;
+    SYSCTL_RCC_R = (SYSCTL_RCC_R & ~0x000007C0) + 0x00000540;
+    SYSCTL_RCC2_R &= ~0x00000070;
+    SYSCTL_RCC2_R &= ~0x00002000;
+    SYSCTL_RCC2_R |= 0x40000000;
+    SYSCTL_RCC2_R = (SYSCTL_RCC2_R & ~0x1FC00000) + (4 << 22);
+    while ((SYSCTL_RIS_R & 0x00000040) == 0)
+    {
+    };
+    SYSCTL_RCC2_R &= ~0x00000800;
+}
+
+/*
+ * main.c
+ */
+int main(void)
+{
+    PLLInit();
+    SystickInit();
+    SetupSerial();
+
+    SerialWriteLine("Go");
+
+    while (1)
+    {
+
+    }
+}
+
